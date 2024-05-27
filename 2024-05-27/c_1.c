e6631eda13dde4a0ca895f52cdcb7068f7c0115c
tohsaka888 <156132264@qq.com>
2024-05-27 16:52:38 +0800

    fix: 错误的读取输入

--- 2024-05-27/c_1.c
+++ 2024-05-27/c_1.c
@@ -5,15 +5,18 @@
 
 int main() {
     char buf[BUFFSIZE];
+    int n;
 
     // STDIN_FILENO和STDOUT_FILENO是POSIX标准的一部分
-    int n = read(STDIN_FILENO, buf, BUFFSIZE);
-    while (n > 0) {
+    // FIX：每次循环都要读取标准输入
+    while ((n = read(STDIN_FILENO, buf, BUFFSIZE)) > 0) {
+        // 写入的数据如果小于读取的数据，说明写入失败
+        // 在“标准输出”中写入“标准输入”的数据
         if (write(STDIN_FILENO, buf, n) != n) {
             perror("write error");
         }
     }
-
+    // 没读取到输入
     if (n < 0) {
         perror("read error");
     }
