87231e91db11b6981b3c19123783c82959f4030c
EtherLC <1528661882@qq.com>
2024-05-28 13:40:53 +0800

    fix:更改输出，使其符合ls的输出状态

--- Chapter_1/1_4.c
+++ Chapter_1/1_4.c
@@ -15,7 +15,7 @@ int main(int argc,char *argv[])
 
     if (argc != 2)
     {
-        err_quit("usage:is directory_name");
+        err_quit("usage:ls directory_name");
     }
 
     if ((DP = opendir(argv[1]))==NULL)  //argv[1]是程序运行时传入的第二个参数，即要打开的目录名，"args": ["."],代表当前目录下的所有文件
@@ -25,9 +25,10 @@ int main(int argc,char *argv[])
     }
     while ((dirp = readdir(DP))!=NULL)  //用于读取目录中的下一个文件名
     {
-        printf("%s\n",dirp->d_name);
+        printf("%s ",dirp->d_name);
     }
 
+    printf("\n ");
     closedir(DP);  //关闭先前由 opendir() 打开的目录
     exit(0);
 }
