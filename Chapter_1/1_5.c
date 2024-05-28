4dbd13b551c972f6959daa1981e354e18c83058e
EtherLC <1528661882@qq.com>
2024-05-28 14:35:57 +0800

    fix:标准输入复制到标准输出

--- Chapter_1/1_5.c
+++ Chapter_1/1_5.c
@@ -5,20 +5,23 @@
 
 #define buffsize 4096
 
-int main(void) {
-    int n;
-    int m;
-    int buff[buffsize];
+int main(void)
+{
+	int n;
+	int m;
+	int buff[buffsize];
 
-    n = read(STDIN_FILENO, buff,sizeof(buff));  // 使用 STDIN_FILENO 作为参数来读取标准输入流的数据
-    m = write(STDOUT_FILENO, buff, n);    // 使用 STDOUT_FILENO 作为参数来将消息写入到标准输出流。
-    if (n > 0) {
-        if (m !=n) 
-        {  
-            perror("write error");
-        }
-    }
-    if (n < 0) {
-        perror("read error");
-    }
+	n = read(STDIN_FILENO, buff, sizeof(buff)); // 使用 STDIN_FILENO 作为参数来读取标准输入流的数据
+	m = write(STDOUT_FILENO, buff, n); // 使用 STDOUT_FILENO 作为参数来将消息写入到标准输出流。
+	if(n > 0)
+	{
+		if(m != n)
+		{
+			perror("write error");
+		}
+	}
+	if(n < 0)
+	{
+		perror("read error");
+	}
 }
